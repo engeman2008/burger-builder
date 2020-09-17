6da804a7f2113c71280918104a721c61cb7001ce
Eman <eman.cse2008@gmail.com>
2020-09-17 11:40:51 +0200

    add burger comp and modal with backdrop

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+
+];
+
+const buildControls = (props) => (
+    <div className={styles.BuildControls}>
+        <p>Current price: {props.price.toFixed(2)}</p>
+        {controls.map((ctrl) => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                type={ctrl.type}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
+        ))}
+        <button 
+        className={styles.OrderButton}
+        disabled={!props.purchaseable}
+        onClick={props.ordered} >ORDER NOW</button>
+    </div>
+);
+
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transfomedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transfomedIngredients.length === 0) {
+        transfomedIngredients = <p>Please start adding ingredients</p>
+    }
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type='bread-top' />
+            {transfomedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+    );
+};
+
+export default burger;
