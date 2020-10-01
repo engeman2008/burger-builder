ed70e322609ffdfd460df8dcfdc49d2c6744646d
Eman <eman.cse2008@gmail.com>
2020-10-01 10:19:20 +0200

    add logout saga

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -12,15 +13,19 @@ import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { logoutSaga } from './store/sagas/auth';
 
 const composeEnhancers = composeWithDevTools({});
+const sagaMiddleware = createSagaMiddleware()
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer
 });
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+
+sagaMiddleware.run(logoutSaga);
 
 ReactDOM.render(
   <React.StrictMode>
